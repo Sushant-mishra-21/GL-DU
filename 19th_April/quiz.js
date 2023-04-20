393dc5d1a7ec3454c3b6884690bd480513fcf110
Sushant-mishra-21 <sushant.mishra@globallogic.com>
2023-04-20 15:32:06 +0530

    initalized the landing page

--- 19th_April/quiz.js
+++ 19th_April/quiz.js
@@ -1,50 +1,36 @@
-const quizForm = document.getElementById("quiz-form");
-const timer = document.getElementById("timer");
-const result = document.getElementById("result");
-
 let score = 0;
-let timeLeft = 30; // 10 minutes in seconds
+let timeleft = 2225;
+const timer = document.querySelector("#timer");
 
-// Start the timer
 const interval = setInterval(() => {
-  timeLeft--;
-  timer.innerHTML = `Time left: ${timeLeft} seconds`;
-
-  if (timeLeft === 0) {
+  timeleft--;
+  if (timeleft > 60) {
+    let t_output_m = Math.floor(timeleft / 60);
+    let t_output_s = Math.floor(timeleft % 60);
+    timer.innerHTML = `Time left : ${t_output_m} m ${t_output_s} s`;
+  } else if (timeleft < 15) {
+    timer.style.color = "red";
+    timer.innerHTML = `Time left : ${timeleft} s`;
+  } else {
+    timer.innerHTML = `Time left : ${timeleft} s`;
+  }
+  if (timeleft == 0) {
     clearInterval(interval);
-    disableForm();
-    showResult();
+    timer.innerHTML = `Time up!`;
   }
 }, 1000);
-
-// Disable the form once the timer expires
-function disableForm() {
-  for (let i = 0; i < quizForm.elements.length; i++) {
-    quizForm.elements[i].disabled = true;
-  }
+const getData =async ()=>{
+  const res = await fetch(
+  "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium"
+)
+  // .then((Response) => Response.json())
+  // .then((data) => {
+  //   console.log(data);
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  // });
+  const data = await res.json();
+  console.log(data.results)
 }
-
-// Calculate the score and show the result
-function showResult() {
-  for (let i = 0; i < quizForm.elements.length; i++) {
-    const element = quizForm.elements[i];
-
-    if (element.checked) {
-      // Check if the selected answer is correct
-      if (element.value === "b") {
-        score++;
-      }
-    }
-  }
-
-  result.innerHTML = `You scored ${score} out of 10!`;
-}
-
-// Submit the quiz
-quizForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  disableForm();
-  clearInterval(interval);
-  showResult();
-});
+getData()
