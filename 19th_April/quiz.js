0f60504f155fff703d53d7fc26de1329c34cf11e
Sushant-mishra-21 <Sushant.mishra@globallogic.com>
2023-04-21 00:02:39 +0530

    added questions display functionality

--- 19th_April/quiz.js
+++ 19th_April/quiz.js
@@ -1,7 +1,14 @@
 let score = 0;
 let timeleft = 25;
 const timer = document.querySelector("#timer");
-let count =0;  
+const question = document.querySelector("#question");
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+const option4 = document.querySelector("#option4");
+const button = document.querySelector(".btn-sub");
+let count = 0;
+let dataLength = 0;
 const interval = setInterval(() => {
   timeleft--;
   if (timeleft > 60) {
@@ -19,14 +26,33 @@ const interval = setInterval(() => {
     timer.innerHTML = `Time up!`;
   }
 }, 1000);
-const getData =async ()=>{
-  
+const getData = async () => {
   const res = await fetch(
-  "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium"
-)
-  
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium"
+  );
+
   const data = await res.json();
-  console.log(data.results[count].question)
+  question.innerHTML = data.results[count].question;
+  option1.innerHTML = data.results[count].correct_answer;
+  option2.innerHTML = data.results[count].incorrect_answers[0];
+  option3.innerHTML = data.results[count].incorrect_answers[1];
+  option4.innerHTML = data.results[count].incorrect_answers[2];
+  console.log(data.results);
+  dataLength = data.results.length;
+  console.log(dataLength);
+};
+getData();
+
+function buttonClicked() {
   count++;
+  if (count <= dataLength) {
+    getData();
+  } else {
+    testCompleted();
+  }
 }
-getData()
+function testCompleted() {
+  button.disabled = true;
+  alert("Test Finished!");
+  window.location.href = "TestFinished.html";
+}
